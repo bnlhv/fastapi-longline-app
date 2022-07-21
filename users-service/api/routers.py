d2b9e236bec2eb8c716ee85599afed4d039e9f84
Ben Lahav <benlahav1990@gmail.com>
2022-07-21 08:37:35 +0300

    Added routes

--- users-service/api/routers.py
+++ users-service/api/routers.py
@@ -1,8 +1,10 @@
 """ Module for all services route endpoints """
+from typing import List
 
 from fastapi import APIRouter, status
 
-from services import *
+import services
+from models import UserIn_Pydantic, UserOut_Pydantic
 
 user_router = APIRouter(prefix="/users")
 
@@ -12,5 +14,32 @@ user_router = APIRouter(prefix="/users")
     status_code=status.HTTP_200_OK,
     response_model=UserOut_Pydantic,
 )
-def get_users() -> List[UserOut_Pydantic]:
-    return await get_all_users()
+async def get_users() -> List[UserOut_Pydantic]:
+    return await services.get_all_users()
+
+
+@user_router.post(
+    path="/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserOut_Pydantic
+)
+async def create_user(user: UserIn_Pydantic) -> List[UserOut_Pydantic]:
+    return await services.create_user(user)
+
+
+@user_router.get(
+    path="/{user_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=UserOut_Pydantic,
+)
+async def get_user(user_id: int) -> UserOut_Pydantic:
+    return await services.get_user(user_id)
+
+
+@user_router.put(
+    path="/{user_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=UserOut_Pydantic,
+)
+async def update_user(user_id: int, user: UserIn_Pydantic) -> UserOut_Pydantic:
+    return await services.update_user(user_id=user_id, data=user)
